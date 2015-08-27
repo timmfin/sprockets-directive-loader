d05187df4d619b6eea7d1955b22514dafb796673
Tim Finley <tfinley@hubspot.com>
2015-08-27 10:15:39 -0400

    First commit

---
+++ Readme.md
@@ -0,0 +1,37 @@
+# Sprockets-style directive loader for webpack
+
+Converts [Sprockets/Rails directives](https://github.com/sstephenson/sprockets#the-directive-processor)—things like `//= require <path>`—into modules recognizable by Webpack. Works on JS and CSS files.
+
+### Caveats
+
+  1. This loader assumes that every JS file you `//= require` is a complete, valid JavaScript file that can run on its own. In Rails, you could concatenate together files that were each not valid JavaScript syntax (for example, three files like this: `function sillySplitFunction() {`, `console.log('in silly function');`, `}  // end silly function`).
+  2. This loader doesn't do automatic extension conversion for you. Sprockets/Rails would allow you to refer to `//= require file.coffee` or `//= require file.js`, but in this loader you'll need to refer to exactly the extension that is on the file system (at the point the loader runs?).
+
+_Note it may be possible to fix some or all of the above, but this is only version `0.0.x` and I'm trying the simplest approach first._
+
+## Usage
+
+[Documentation: Using loaders](http://webpack.github.io/docs/using-loaders.html)
+
+## Examples
+
+``` javascript
+{
+  // ...
+
+  module: {
+    // Needs to be a pre-loader so that it is run before files are convered to JS
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader:  "sprockets-directive-loader"
+      }, {
+        test: /\.css$/,
+        loader: "sprockets-directive-loader"
+      }
+    ],
+
+    // ...
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,81 @@
+var path = require('path');
+var cloneRegexp = require('clone-regexp');
+var shellwords = require('shellwords');
+var pascalCase = require('pascal-case');
+var loaderUtils = require('loader-utils');
+
+
+// Copied from:
+// https://github.com/timmfin/broccoli-sprockets-dependencies/blob/4f20be3924d93577c156b3385fc9f2900af6a595/resolver.coffee#L13-L34
+
+var HEADER_PATTERN = /^(?:\s*((?:\/[*](?:\s*|.+?)*?[*]\/)|(?:\#\#\#\n(?:[\s\S]*)\n\#\#\#)|(?:\/\/.*\n?)+|(?:\#.*\n?)+)*)*/m;
+var DIRECTIVE_PATTERN = /^(\W*=)\s*(\w+)\s*(.*?)(\*\/)?$/gm;
+
+
+var DirectiveMethods = {
+  processRequireDirective: function(webpackLoader, state, arg1) {
+    state.pathsToRequire.push(arg1);
+  }
+}
+
+
+function extractHeader(content, customHeaderPattern) {
+  var headerPattern = cloneRegexp(customHeaderPattern || HEADER_PATTERN);
+
+  // Must be at the very beginning of the file
+  if ((match = headerPattern.exec(content)) && match && match.index === 0) {
+    return match[0]
+  }
+}
+
+function processDependenciesInContent(webpackLoader, content) {
+  var isCSS = /\.css$/.test(webpackLoader.resourcePath);
+  webpackLoader.cacheable(true);
+
+  // Extract out all the directives from the header (directives can only appear
+  // at the top of the file)
+  var header = extractHeader(content);
+
+  // clone regex for safety
+  var directivePattern = cloneRegexp(DIRECTIVE_PATTERN);
+
+  var state = {
+    pathsToRequire: []
+  };
+
+  while (match = directivePattern.exec(header)) {
+    var preDirective  = match[1];
+    var directive     = match[2];
+    var directiveArgs = shellwords.split(match[3]);
+
+    var directiveFunc = "process" + pascalCase(directive) + "Directive";
+
+    if (DirectiveMethods[directiveFunc]) {
+      DirectiveMethods[directiveFunc].apply(this, [webpackLoader, state].concat(directiveArgs));
+    } else {
+      console.warn("Potentially unknown directive `" + preDirective.trim() + " " + directive + "` ? (found in " + resourcePath + ")");
+    }
+  }
+
+  if (state.pathsToRequire.length > 0) {
+
+    var modifiedHeader = state.pathsToRequire.map(function(pathToRequire) {
+      if (isCSS) {
+        return "@import url(" + loaderUtils.stringifyRequest(webpackLoader, pathToRequire) + ");"
+      } else {
+        return "require(" + loaderUtils.stringifyRequest(webpackLoader, pathToRequire) + ");"
+      }
+    }).join('\n');
+
+    content = '/* Start webpack directive-loader modifications */\n' + content.replace(header, modifiedHeader + '\n' + '/* End webpack directive-loader modifications */\n\n');
+  }
+
+  return content;
+}
+
+
+
+module.exports = function(source) {
+  var modifiedSource = processDependenciesInContent(this, source)
+  return modifiedSource;
+};
