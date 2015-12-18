fad94c51899a01139f157dc143b6304a83cde300
Tim Finley <tfinley@hubspot.com>
2015-12-18 17:01:09 -0500

    Get require_tree and require_directory to work.
    Needs refactoring.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var path = require('path');
 var cloneRegexp = require('clone-regexp');
 var shellwords = require('shellwords');
@@ -10,6 +11,56 @@ var loaderUtils = require('loader-utils');
 
 var HEADER_PATTERN = /^(?:\s*((?:\/[*](?:\s*|.+?)*?[*]\/)|(?:\#\#\#\n(?:[\s\S]*)\n\#\#\#)|(?:\/\/.*\n?)+|(?:\#.*\n?)+)*)*/m;
 var DIRECTIVE_PATTERN = /^(\W*=)\s*(\w+)\s*(.*?)(\*\/)?$/gm;
+
+
+function ensureDirDoesntStartWithASlash(dir) {
+  // Remove front slash
+  if (dir[0] === '/') {
+    dir = dir.slice(1);
+  }
+
+  return dir;
+}
+
+
+function lookUpDirectoryIn(dirToLookup, rootPathsToCheck) {
+  dirToLookup = ensureDirDoesntStartWithASlash(dirToLookup);
+
+  console.log("rootPathsToCheck", rootPathsToCheck);
+  for (var i = 0; i < rootPathsToCheck.length; i++) {
+    var fullPathToCheck = path.join(rootPathsToCheck[i], dirToLookup);
+
+    try {
+      // TODO, make async
+      var stats = fs.statSync(fullPathToCheck);
+
+      if (stats.isDirectory()) {
+        return fullPathToCheck;
+      }
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+    }
+  }
+
+  return;
+}
+
+
+function buildContextRequireString(resolvedDir, extensions, options) {
+  options = options || {};
+
+  return [
+    'var req = require.context(',
+      JSON.stringify(resolvedDir) + ',',
+      (options.recursive === true) + ',',  // include subdirectories
+      '/.*\.(' + extensions.join('|') + ')$/',
+    '); req.keys().forEach(function(key){',
+      'req(key);',
+    '});'
+  ].join('\n');
+}
 
 
 var DirectiveMethods = {
@@ -28,6 +79,68 @@ var DirectiveMethods = {
       return importStr;
     } else {
       return "require(" + loaderUtils.stringifyRequest(webpackLoader, pathToRequire) + ");"
+    }
+  },
+
+  processRequireTreeDirective: function(webpackLoader, meta, dirToRequire) {
+    if (meta.isCss) {
+      throw new Error("Can't do require_tree for CSS yet");
+    } else {
+      var resolvedDir,
+          locationsToLookIn = [ webpackLoader.options.context];
+
+      if (webpackLoader.options.resolve && webpackLoader.options.resolve.root) {
+        Array.prototype.push.apply(locationsToLookIn, webpackLoader.options.resolve.root);
+      }
+
+      // If the dirToRequire is a relative path
+      if (/^\.?\/\//.test(dirToRequire)) {
+        throw new Error("You cannot (yet?) use relative paths in require_tree");
+        // CONSIDER, allow??? And ensure that the final resolved path is inside
+        // locationsToLookIn ?
+      } else {
+        resolvedDir = lookUpDirectoryIn(dirToRequire, locationsToLookIn);
+
+        if (!resolvedDir) {
+          throw new Error("Couldn't find " + dirToRequire + " in any of " + locationsToLookIn.join(', '))
+        }
+
+      }
+
+      return buildContextRequireString(resolvedDir, ['js', 'coffee'], {
+        recursive: true
+      });
+    }
+  },
+
+  processRequireDirectoryDirective: function(webpackLoader, meta, dirToRequire) {
+    if (meta.isCss) {
+      throw new Error("Can't do require_directory for CSS yet");
+    } else {
+      var resolvedDir,
+          locationsToLookIn = [ webpackLoader.options.context];
+
+      if (webpackLoader.options.resolve && webpackLoader.options.resolve.root) {
+        Array.prototype.push.apply(locationsToLookIn, webpackLoader.options.resolve.root);
+      }
+
+      // If the dirToRequire is a relative path
+      if (/^\.?\/\//.test(dirToRequire)) {
+        throw new Error("You cannot (yet?) use relative paths in require_directory");
+        // CONSIDER, allow??? And ensure that the final resolved path is inside
+        // locationsToLookIn ?
+      } else {
+        resolvedDir = lookUpDirectoryIn(dirToRequire, locationsToLookIn);
+
+        if (!resolvedDir) {
+          throw new Error("Couldn't find " + dirToRequire + " in any of " + locationsToLookIn.join(', '))
+        }
+
+      }
+
+      return buildContextRequireString(resolvedDir, ['js', 'coffee'], {
+        recursive: false
+      });
     }
   }
 }
